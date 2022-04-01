b6f1a2ca65b8494454d9f490cf9782d10c973537
abe12hudekimo <102787559+abe12hudekimo@users.noreply.github.com>
2022-04-01 16:02:28 +0900

    Initial commit
    打刻アプリ用です。

---
+++ index.js
@@ -0,0 +1,26 @@
+let unko = "hello world";
+
+
+
+//定数
+const big_unko = "hello world2";
+
+let inoki = ["1","2","3","daaa"];
+
+console.log(inoki[0]);
+
+//ループ文
+//let index = 0;
+//while(index< inoki. length){
+// 繰り返したい説明
+//console. log(inokis[index]);
+//index++;
+//}
+
+if(inoki.length > 3){
+  console.log('booooo');
+} else{
+  console.log('boo');
+
+
+}
